b98dbd6aef7bb988335850d06ad4ea82b83dda2a
carodriguez836 <135251405+crodriguezchaparro2@users.noreply.github.com>
2023-06-20 08:37:31 -0500

    Update

---
+++ error505.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/styles4.css">
+  <title>Error 505</title>
+</head>
+<body>
+  <header>
+    <div class="container">
+      <img src="imagenes/1.jpg" alt="logo">
+    </div>
+  </header>
+  <div id="clouds">
+    <div class="cloud x1"></div>
+    <div class="cloud x1_5"></div>
+    <div class="cloud x2"></div>
+    <div class="cloud x3"></div>
+    <div class="cloud x4"></div>
+    <div class="cloud x5"></div>
+  </div>
+  <div class='c'>
+    <div class='_404'>500</div>
+    <hr>
+    <div class='_1'>pagina no </div>
+    <br>
+    <div class='_1'>encontrada </div>
+    <br>
+    <a class='btn' href='#'>volver al inicio</a>
+  </div>
+</body>
+</html>
