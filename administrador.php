ae96919955cb1f6547b68d263872f249f455ab5c
carodriguez836 <135251405+crodriguezchaparro2@users.noreply.github.com>
2023-06-24 17:39:50 -0500

    update

--- administrador.php
+++ administrador.php
@@ -1,9 +1,10 @@
+<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/styles.css">
+    <link rel="stylesheet" href="CSS/styles6.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/b408879b64.js" crossorigin="anonymous"></script>
@@ -14,17 +15,24 @@
 <body>
     <header>
         <div class="container">
-            <img src="imagenes/1.jpg" alt="logo">
-            <div class="buscar">
-                <input type="text" placeholder="Buscar...">
-                <button><i class="fa-solid fa-magnifying-glass"></i></button>
+            <div class="btn-menu">
+                <label for="btn-menu" class="fa-solid fa-bars"></label>
             </div>
-            <input type="submit" value="Iniciar sesion" onclick="window.location.href='login.php'">
-            <nav>
-                <i class="fa-solid fa-cart-shopping"></i>
-            </nav>
+            <img src="imagenes/1.jpg" alt="logo">
+            <h1>Bienvenido administrador</h1>
+            <input type="submit" value="Salir" onclick="window.location.href='inicio.php'">
         </div>
     </header>
+    <input type="checkbox" id="btn-menu">
+    <div class="container-menu">
+        <div class="cont-menu">
+            <nav>
+                <a href="#">Inventarios</a>
+                <a href="#">Ventas</a>
+            </nav>
+            <label for="btn-menu"class="fa-solid fa-xmark"></label>
+        </div>
+    </div>
     <header>
     <nav class="nav-secundario">
         <div class="link-secundarios izquierda">
